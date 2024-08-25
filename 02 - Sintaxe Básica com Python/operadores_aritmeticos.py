926c1e6a3a7c5c468cfe492ced7b93d4603f3464
Raquel Rodrigues <raquelrodrigues@grupogelog.com.br>
2024-08-25 11:28:14 -0300

    Inicio Modulo 02

---
+++ 02 - Sintaxe Básica com Python/operadores_aritmeticos.py
@@ -0,0 +1,20 @@
+# adição
+print(2 + 2)
+
+# subtração
+print(2 - 2)
+
+# multiplicação
+print(2 * 2)
+
+# divisão
+print(2 / 2)
+
+# divisão inteira
+print(2 // 2)
+
+# resto da divisão (modulo)
+print(2 % 2)
+
+# exponenciação
+print(2 ** 2)
