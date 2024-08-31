697c644a7e6554e2795534e94ff203ff8698512f
Raquel Rodrigues <raquelrodrigues@grupogelog.com.br>
2024-08-31 11:40:12 -0300

    Modulo 01 Finalizado

--- 01 - Introdução ao Ambiente Python/02 - Conhecendo a Linguagem/02_variaveis_constantes.py
+++ 01 - Introdução ao Ambiente Python/02 - Conhecendo a Linguagem/02_variaveis_constantes.py
@@ -6,7 +6,7 @@ print(f'Meu nome é {nome} e eu tenho {idade} ano(s) de idade.')
 idade, nome = (21, 'Raquel Rodrigues')
 print(f'Meu nome é {nome} e eu tenho {idade} ano(s) de idade.')
 
-# constantes
+# constantes*
 ## Python não possui um tipo de dado específico para constantes, mas por convenção, variáveis que não devem ser alteradas são escritas em letras maiúsculas
 PI = 3.14159
 print(PI)
